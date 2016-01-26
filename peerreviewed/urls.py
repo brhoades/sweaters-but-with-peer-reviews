c207a205af316bdf9209ce9b87bbbe9c93526a86
Billy Rhoades <billyarhoades@gmail.com>
2016-01-26 12:45:29 -0500

    Base Django repository. Nothing of consequence.

---
+++ peerreviewed/urls.py
@@ -0,0 +1,12 @@
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+from browse.views import index
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'peerreviewed.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', index),
+)
