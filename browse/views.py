1a977369bd38bd56a3a3c0838e948a64aba9abb7
Billy Rhoades <billyarhoades@gmail.com>
2016-01-26 13:57:58 -0500

    IntegralGit test

--- browse/views.py
+++ browse/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<title>Sweaters But With Peer Reviews</title>Sam Sucks")
+    return HttpResponse("<title>Sweaters But With Peer Reviews</title><h3>Sam Sucks</h3>")
