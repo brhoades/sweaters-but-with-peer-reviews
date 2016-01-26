a3d228709599690748d9cea302de7d8ddd72470c
Billy Rhoades <billyarhoades@gmail.com>
2016-01-26 13:28:53 -0600

    Moved settings to per-user. Added gitignore for it

--- peerreviewed/settings.example.py
+++ peerreviewed/settings.example.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'xtr#v+@=g5u*p$is(wzb+w&-i1q5kga6b(3o#-u=-vclhto+3m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
