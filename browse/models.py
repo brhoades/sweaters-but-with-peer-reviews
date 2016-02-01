f745c5290dd51fe5463cf9296ccfbabbb167c865
Billy Rhoades <billyarhoades@gmail.com>
2016-02-01 15:45:23 -0600

    Added contrib.models.user to models.

--- browse/models.py
+++ browse/models.py
@@ -1,25 +1,31 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.contrib.auth.models import User
 
-class Review(models.Model):
-	# source = models.ForeignKey(Users)
-	professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
-	course = models.CharField(max_length=100)
-	created_ts = models.DateTimeField(auto_now_add=True)
-	updated_ts = models.DateTimeField(auto_now=True)
-
-class Professor(models.Model):
-	owner = models.ForeignKey(Users)
-	first_name = models.CharField(max_length=50)
-	last_name = models.CharField(max_length=50)
-	school = models.ForeignKey(School)
 
 class School(models.Model):
-	name = models.CharField(max_length=100)
-	email_pattern = models.CharField(max_length=50)
-	location = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    email_pattern = models.CharField(max_length=50)
+    location = models.CharField(max_length=100)
+
+
+class Professor(models.Model):
+    owner = models.ForeignKey(User)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    school = models.ForeignKey(School)
+
+
+class Review(models.Model):
+    # source = models.ForeignKey(Users)
+    professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
+    course = models.CharField(max_length=100)
+    created_ts = models.DateTimeField(auto_now_add=True)
+    updated_ts = models.DateTimeField(auto_now=True)
+
 
 class Review_Votes(models.Model):
-	quality = models.IntegerField(validators=[MaxValueValidator(100),MinValueValidator(0)])
-	review = models.ForeignKey(Review)
-	# reviewer = models.ForeignKey(Users)
+    quality = models.IntegerField(validators=[MaxValueValidator(100),
+                                  MinValueValidator(0)])
+    review = models.ForeignKey(Review)
+    reviewer = models.ForeignKey(User)
