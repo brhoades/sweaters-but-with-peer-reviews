60271dd7cbb1c2eae8a7c3081b4da77a293be6c8
Billy Rhoades <billyarhoades@gmail.com>
2016-02-01 16:22:50 -0600

    Added all models to admin cp.

--- browse/admin.py
+++ browse/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from browse.models import School, Professor, Review, Review_Votes
+
 
 # Register your models here.
+admin.site.register(School)
+admin.site.register(Professor)
+admin.site.register(Review)
+admin.site.register(Review_Votes)
+
