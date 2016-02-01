eb7f5d05b31fc519551f08945d316d5961273e82
Billy Rhoades <billyarhoades@gmail.com>
2016-02-01 11:13:23 -0500

    Added example sensitive file.

---
+++ peerreviewed/sensitive.example.py
@@ -0,0 +1,19 @@
+# SECURITY WARNING: keep the secret key used in production secret!
+# Turn this into a string.
+SECRET_KEY = None
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/1.7/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
