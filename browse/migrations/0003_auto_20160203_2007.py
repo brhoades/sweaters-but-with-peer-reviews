a237bad10f904bae7a289346ea576ebbc9911aec
Billy Rhoades <billyarhoades@gmail.com>
2016-02-03 14:25:45 -0600

    Fix for nobody user, which doesn't work.

--- browse/migrations/0003_auto_20160203_2007.py
+++ browse/migrations/0003_auto_20160203_2007.py
@@ -60,7 +60,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='professor',
             name='created_by',
-            field=models.ForeignKey(default='nobody', on_delete=django.db.models.deletion.CASCADE, related_name='created_by', to=settings.AUTH_USER_MODEL),
+            field=models.ForeignKey(default=0, on_delete=django.db.models.deletion.CASCADE, related_name='created_by', to=settings.AUTH_USER_MODEL),
             preserve_default=False,
         ),
         migrations.AddField(
