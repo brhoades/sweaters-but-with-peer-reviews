80f68dd19125d0f42c9c7650574c9b2c9ed0e613
Dakota Ewigman <dlefh7@mst.edu>
2016-04-04 21:00:58 -0500

    improved messages to users

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,6 +7,6 @@ from accounts.views import email_sent, register_confirm
 
 urlpatterns = [
     url(r'^register/', CreateView.as_view(template_name='accounts/register.html', form_class=UserCreateForm, success_url='/accounts/email_sent/'), name="user_registration"),
-	url(r'^email_sent/', email_sent, name='email_sent'),
+        url(r'^email_sent/', email_sent, name="email_sent"),
 	url(r'^confirm/(?P<activation_key>\w+)/', register_confirm, name='register_confirm'),
 ]
